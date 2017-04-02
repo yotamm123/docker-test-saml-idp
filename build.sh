63cca188b819f8396b7fd900b1b1fff143a2065f
yotamm123 <yotam@insert.io>
2017-04-02 16:33:42 +0300

    change configuration to be more keycloak friendly

---
+++ build.sh
@@ -0,0 +1,1 @@
+docker build -t yms/samlidp:2.0 .
--- config/simplesamlphp/config.php
+++ config/simplesamlphp/config.php
@@ -87,7 +87,7 @@ $config = array(
      * A possible way to generate a random salt is by running the following command from a unix shell:
      * tr -c -d '0123456789abcdefghijklmnopqrstuvwxyz' </dev/urandom | dd bs=32 count=1 2>/dev/null;echo
      */
-    'secretsalt' => ((getenv('SIMPLESAMLPHP_SECRET_SALT') != '') ? getenv('SIMPLESAMLPHP_SECRET_SALT') : 'defaultsecretsalt'),
+    'secretsalt' => 'pooooopp',
 
     /*
      * Some information about the technical persons running this installation.
